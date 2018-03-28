6c1b8456db1c24cb98d295ec3116b51e6e29942d
CHIXUEZI <35863737+CHIXUEZI@users.noreply.github.com>
2018-03-28 12:21:39 +0800

    math

--- mathquater/js/all.js
+++ mathquater/js/all.js
@@ -1,8 +1,19 @@
 $(function() {
-            $(".nav_li").hover(function() {
+    $(".nav_li").hover(function() {
 
-                $(this).children("ul").slideDown(400);
-            }, function() {
-                $(this).children("ul").slideUp(100);
-            });
-        });
+        $(this).children("ul").slideDown(400);
+    }, function() {
+        $(this).children("ul").slideUp(100);
+    });
+    var url = window.location.href;
+    $('.newslist #newslist_main .newslist_title ul li').each(function() {
+        if (returnUrl($(this).children('a').attr('href')) == returnUrl(url)) {
+            $(this).addClass('active');
+        }
+    });
+
+    function returnUrl(href) {
+        var unum = href.lastIndexOf('/');
+        return href.substring(unum + 1);
+    }
+});
