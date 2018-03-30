e3b1fac3f010e25a0030278c419da46f27647815
CHIXUEZI <994498828@qq.com>
2018-03-30 13:54:02 +0800

    html

--- xkjs/js/public.js
+++ xkjs/js/public.js
@@ -1,4 +1,5 @@
 ;$(function(){
+	changebackimg();
 	$('.nav1 > li').hover(function() {
 		/* Stuff to do when the mouse enters the element */
 		$(this).children('.nav2').slideDown('slow');
@@ -12,8 +13,31 @@
 			$(this).addClass('active');
 		}
 	});
+	// newsimg轮播
+	var banimgs = $('.section .secmain #newsimg ul li a');
+	var count =0;
+	var time=setInterval(newsbanner,2400);
+	function newsbanner(){
+		banimgs.eq(count).fadeIn().siblings().fadeOut();
+		if(count>=banimgs.length-1){
+			count=0;
+		}else{
+			count++;
+		}
+		alert(banimgs.eq(count).siblings());
+	}
+	// action点击切换
 	function returnUrl(href){
 		var unum = href.lastIndexOf('/');
 		return href.substring(unum+1);
+	};
+
+	// 随机切换look
+	function changebackimg(){
+		var random_imgs = Math.floor(Math.random()*3+1);
+		var bimgUrl = 'url(./images/rod/'+random_imgs+'.jpg)';
+		//alert(bimgUrl);
+		$('#look').css("background-image",bimgUrl);
 	}
+
 });
