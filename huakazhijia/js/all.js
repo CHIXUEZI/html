9ea5b629ea21939ecc5dbf808542bcd9909bcd2f
CHIXUEZI <994498828@qq.com>
2018-04-04 16:02:55 +0800

    html

--- huakazhijia/js/all.js
+++ huakazhijia/js/all.js
@@ -1,9 +1,18 @@
-$(function () {
-	$('#header_main ul li').click(function() {
-		$(this).addClass('active').siblings().removeClass('active');
-		var count = $(this).index();
-		//var imgname = $(".class_img")[0].src();
-		var path = "src="+"img/icon/"+count+"_xz"+".png";
-		alert(count);
-	});
+$(function() {
+    // 点击切换主界面图片
+    $('#header_main ul li').on("tap", function() {
+        var imgs = $('#header_main ul li a img');
+        for (var i = 0; i < imgs.length; i++) {
+            var j = i + 1;
+            var path_before = "img/icon/" + j + ".png";
+            imgs.eq(i).attr('src', path_before);
+        $('#header_main ul li a').children('p').css('color', '#4B4B4B');
+            //alert(i);
+        }
+        var count = $(this).index() + 1;
+        var imgSrc = $(this).find('img');
+        var path = "img/icon/" + count + "_xz" + ".png";
+        imgSrc.attr('src', path);
+        $(this).find('p').css('color', '#0084FF');
+    });
 });
