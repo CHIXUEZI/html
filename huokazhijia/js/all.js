88436eea35ae6166ec0bb12f41013ace171e6148
CHIXUEZI <35863737+CHIXUEZI@users.noreply.github.com>
2018-04-13 17:55:18 +0800

    huokazhijia

--- huokazhijia/js/all.js
+++ huokazhijia/js/all.js
@@ -1,6 +1,5 @@
 $(function() {
     // 点击切换主界面图片
-
     var imgs = $('#header_main ul li a img');
     for (var i = 0; i < imgs.length; i++) {
         var j = i + 1;
@@ -9,12 +8,12 @@ $(function() {
         $('#header_main ul li a').children('p').css('color', '#4B4B4B');
     }
     var url = window.location.href;
-    console.log($('#header_main ul li').length);
+    //console.log($('#header_main ul li').length);
     $('#header_main ul li').each(function () {
         if(returnUrl($(this).children('a').attr('href'))==returnUrl(url)){
             var imgSrc = $(this).find('img');
             var oldsrc_num = imgSrc.attr('src').lastIndexOf('.');
-            console.log(oldsrc_num);
+            //console.log(oldsrc_num);
             var oldsrc = imgSrc.attr('src').substring(0, oldsrc_num);
             var path =oldsrc+'_xz.png';
             imgSrc.attr('src',path);
@@ -26,8 +25,6 @@ $(function() {
         var url_num = href.lastIndexOf('/');
         return href.substring(url_num+1);
     };
-
-
     //签到功能
     $('#signIn').on('tap', 'a', function() {
         //$(this).children().attr('src','img/icon/button_ok.png');
@@ -36,15 +33,26 @@ $(function() {
         var signcount = '已签到 ' + count1 + ' 天';
         $(this).html(signcount);
     });
-    // 点赞功能
-    $('.able_praise').on('tap', function() {
+    //论坛页点赞功能
+    $('.media-list').on('tap','.able_praise', function(e) {
+        e.stopPropagation();
         if ($(this).attr('disabled') == 'disabled') { return; }
         $(this).find('img').attr('src', 'img/icon/praise_down.png');
         var praise_count = parseInt($(this).find('span').html()) + 1;
         $(this).find('span').html(praise_count);
         $(this).attr('disabled', 'disabled');
-        //alert();
-
-
+    });
+    //点击弹出评论框
+    $(document).on('tap','.commentClick',function (e) {
+        e.stopPropagation();
+        $('.commentBox').slideDown(400, function(){$('.commentBox_back').fadeIn();});
+        
+    });
+    //点击遮盖区域隐藏评论区
+    $(document).on('tap','.commentBox_back',function (e) {
+        //console.log(1);
+        e.stopPropagation();
+        $('.commentBox_back').fadeOut();
+        $('.commentBox').slideUp(300);
     });
 });
